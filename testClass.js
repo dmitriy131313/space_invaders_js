cb9e83fa62b16f3c4677e389a87cb38d3ae6cbfe
dmitriy131313 <43450926+dmitriy131313@users.noreply.github.com>
2022-08-02 06:30:43 +0300

    Add files via upload

---
+++ testClass.js
@@ -0,0 +1,17 @@
+class Test
+{
+    constructor(val)
+    {
+        this._val = val;
+    }
+
+    out()
+    {
+        console.log(this._val);
+    }
+
+    setVal(val)
+    {
+        this._val = val;
+    }
+}
