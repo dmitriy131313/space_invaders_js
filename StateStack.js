5bf073e6cfb0dc33df3398bd2711f3e901d6b941
Dmitriy <dmitriy131313@yahoo.com>
2022-08-03 06:31:39 +0300

    update StateStack

--- StateStack.js
+++ StateStack.js
@@ -19,6 +19,7 @@ class StateStack
     #mPendingList;
     #mFactories;
 
+//public:
     constructor()
     {
         this.#mStack = [];
@@ -26,8 +27,56 @@ class StateStack
         this.#mFactories = new Map();
     }
 
+    registerState(state_id, factoryFunction)
+    {
+        this.#mFactories.set(state_id, factoryFunction);
+    }
+
+	update(dt)
+    {
+        // Iterate from top to bottom, stop as soon as update() returns false
+        for (let index = this.#mStack.length - 1; index >= 0; index--) {
+            if (!this.#mStack[index].update(dt)) break;
+        }
+	    this.#applyPendingChanges();
+    }
+
+	draw()
+    {
+        this.#mStack.forEach(function(item, index){item.draw();});
+    }
+
+	handleEvent(event)
+    {
+        // Iterate from top to bottom, stop as soon as handleEvent() returns false
+        for (let index = this.#mStack.length - 1; index >= 0; index--) {
+            if (!this.#mStack[index].handleEvent(event)) break;
+        }
+	    this.#applyPendingChanges();
+    }
+
+	pushState(state_id)
+    {
+
+    }
+
+	popState()
+    {
+
+    }
+		
+    clearStates()
+    {
+
+    }
+
+	isEmpty() // -> bool
+    {
+
+    }
+
 //private:
-    #createState(id) //return state ptr
+    #createState(id) // -> state pointer
     {
 
     }
